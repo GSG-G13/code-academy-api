b29f89b18e1fd91d7ab118c5782c11bbd40bb263
Batrawi <batrawi2000forever@gmail.com>
2023-06-11 00:05:06 +0300

    create file structure for our api relates #1 closes #1

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response} from 'express';
+import { PORT } from './config';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+const app = express();
+app.set('port', PORT || 3000);
+
+app.disable('x-powered-by');
+
+app.use([
+  express.json(),
+  express.urlencoded({ extended: false }),
+  cookieParser(),
+  compression(),
+  cors()
+]);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    error: false,
+    data: {
+      message: 'Happy Hacking! 🚀',
+    }
+  });
+});
+
+export default app;
