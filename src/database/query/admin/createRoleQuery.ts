4f3eb38d28b336cbf31e03f68ccaca49e66715b1
Shatha Amin <72302786+Shatha-Amin@users.noreply.github.com>
2023-06-11 21:50:54 +0300

    Update and rename createRolesQuery.ts to createRoleQuery.ts

--- src/database/query/admin/createRoleQuery.ts
+++ src/database/query/admin/createRoleQuery.ts
@@ -2,7 +2,7 @@ import connection from '../../config/connection';
 import {roleType}  from '../../../utils/types/helpers';
 
 
-const addRolesQuery = ({ user_id, role_id, cohort_id }: roleType)=> {
+const createRoleQuery = ({ user_id, role_id, cohort_id }: roleType)=> {
   const sql = {
     text: 'INSERT INTO user_roles ( user_id, role_id, cohort_id) VALUES ($1, $2, $3) RETURNING user_id, role_id,cohort_id;',
     values: [ user_id, role_id, cohort_id],
@@ -10,4 +10,4 @@ const addRolesQuery = ({ user_id, role_id, cohort_id }: roleType)=> {
   return connection.query(sql);
 };
 
-export default addRolesQuery;
+export default createRoleQuery;
