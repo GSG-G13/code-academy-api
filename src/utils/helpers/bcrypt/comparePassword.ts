15517aea4b161c6e819a4f08edda5a6cc9f58970
Batrawi <batrawi2000forever@gmail.com>
2023-06-11 16:08:56 +0300

    create cryption to hash and compare password

---
+++ src/utils/helpers/bcrypt/comparePassword.ts
@@ -0,0 +1,5 @@
+import bcrypt from 'bcrypt';
+
+const comparePassword = (password:string,hashedPassword:string) => bcrypt.compare(password,hashedPassword);
+
+export default comparePassword;
---
+++ src/utils/helpers/bcrypt/index.ts
@@ -0,0 +1,5 @@
+import hashPassword from "./hashPassword";
+
+import comparePassword from "./comparePassword";
+
+export {hashPassword,comparePassword};
