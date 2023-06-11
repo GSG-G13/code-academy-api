7f7da7d31f1aeb9c02c3f59b6655d2e3aba81f3b
= <shathaamin63@gmail.com>
2023-06-11 20:24:45 +0300

    addUserSchema validation

--- src/utils/validation/auth/addUserSchema.ts
+++ src/utils/validation/auth/addUserSchema.ts
@@ -1,5 +1,13 @@
 import Joi from 'joi';
 const addUserSchema = Joi.object({
+  full_name: Joi.string()
+  .min(3)
+  .max(30)
+  .required()
+  .messages({
+    'string.empty': 'Full name cannot be empty',
+    'any.required': 'Full name is required',
+  }),
   email: Joi.string()
     .email({minDomainSegments: 2,tlds: { allow: ['com', 'net'] },})
     .required()
@@ -8,42 +16,9 @@ const addUserSchema = Joi.object({
       'string.empty': 'Email cannot be empty',
       'any.required': 'Email is required',
     }),
-  password: Joi.string()
-    .min(8)
-    .required()
-    .pattern(
-      new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).+$')
-    )
-    .messages({
-      'string.pattern.base':
-      'Password must include at least one lowercase letter, one uppercase letter, one number, and one special character',
-      'string.min': 'Password must be at least 8 characters long',
-      'string.empty': 'Password cannot be empty',
-      'any.required': 'Password is required',
-    }),
-  full_name: Joi.string()
-    .min(3)
-    .max(30)
-    .required()
-    .messages({
-      'string.empty': 'Full name cannot be empty',
-      'any.required': 'Full name is required',
-    }),
-  username: Joi.string()
-    .min(3)
-    .max(30)
-    .alphanum()
-    .lowercase()
-    .uppercase()
-    .required()
-    .messages({
-      'string.empty': 'Username cannot be empty',
-      'any.required': 'Username is required',
-    }),
+
   cohort_id: Joi.number()
     .integer()
-    .min(1)
-    .max(100)
     .required()
     .messages({
       'number.base': 'Cohort id must be a number',
@@ -52,8 +27,6 @@ const addUserSchema = Joi.object({
     }),
   role_id: Joi.number()
     .integer()
-    .min(1)
-    .max(100)
     .required()
     .messages({
       'number.base': 'Role id must be a number',
