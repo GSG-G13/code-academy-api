9489f64f1d67db95168793b75242c208bbc4976e
= <shathaamin63@gmail.com>
2023-06-11 22:10:46 +0300

    send email by nodemailer

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT, DEV_DB_URL, NODE_ENV } = process.env;
+const { PORT, DEV_DB_URL, NODE_ENV,EMAIL,EMAIL_PASSWORD} = process.env;
 
-export { PORT, DEV_DB_URL, NODE_ENV };
+export { PORT, DEV_DB_URL, NODE_ENV,EMAIL,EMAIL_PASSWORD};
