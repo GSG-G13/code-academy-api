68fd80686c1e87de85516ed86b8f4f62ad0b1771
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-06-13 11:02:20 +0300

    fix: add semicolon

--- src/database/config/fakeData.sql
+++ src/database/config/fakeData.sql
@@ -8,7 +8,7 @@ VALUES
 INSERT INTO users (full_name, username, email, password, avatar, is_active, career_status_id)
 VALUES
   ('John Doe', 'johndoe', 'johndoe@example.com', 'password123', 'avatar1.jpg', true, 1),
-  ('Jane Smith', 'janesmith', 'janesmith@example.com', 'password456', 'avatar2.jpg', true, 2),
+  ('Jane Smith', 'janesmith', 'janesmith@example.com', 'password456', 'avatar2.jpg', true, 2);
 
 INSERT INTO posts (content, image, is_public, user_id, cohort_id)
 VALUES
