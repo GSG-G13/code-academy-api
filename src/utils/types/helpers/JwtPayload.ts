b20bc465a5a1c11009e21531bb62950430e4fb8a
Nour-Agha99 <temoagha@gmail.com>
2023-06-13 23:34:32 +0300

    fix: edit name of addUsersController and path

--- src/utils/types/helpers/JwtPayload.ts
+++ src/utils/types/helpers/JwtPayload.ts
@@ -1,14 +1,7 @@
-interface UserRoles {
-  cohortId: number;
-  roleId: number;
-}
-
 interface JwtPayload {
   id: number;
   username: string;
   email: string;
-  isAdmin: boolean;
-  userRoles?: UserRoles[];
 }
 
 export default JwtPayload;
