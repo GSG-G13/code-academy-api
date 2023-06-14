c87715c3b2c3c4508a94a43693bb02d8890bb83b
= <shathaamin63@gmail.com>
2023-06-14 16:32:56 +0300

    fix: edit router delete post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
 import { authRouter, userRouter, cohortsRouter } from './admin';
-import deletePostRouter from './deletePost';
+import postRouter from './posts';
 
 const router: Router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/cohorts', cohortsRouter);
-router.use('/posts/:id', deletePostRouter);
+router.use('/posts', postRouter);
 export default router;
