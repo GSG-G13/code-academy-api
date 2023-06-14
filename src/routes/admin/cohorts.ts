00d545bc13a27f45e976098e69c2fd2a33918388
Nour-Agha99 <temoagha@gmail.com>
2023-06-14 11:35:19 +0300

    fix: checkAuth to routers

--- src/routes/admin/cohorts.ts
+++ src/routes/admin/cohorts.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express';
 import { addCohortController } from '../../controllers';
+import checkAuth from '../../middlewares';
 
 const cohortsRouter: Router = express.Router();
 
+cohortsRouter.use(checkAuth);
 cohortsRouter.post('/', addCohortController);
 
 export default cohortsRouter;
