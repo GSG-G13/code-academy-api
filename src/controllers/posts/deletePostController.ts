871adaf66521fe41b82e80264127c03c0f3b3b76
= <shathaamin63@gmail.com>
2023-06-14 12:55:58 +0300

    feat: add delete post controller

--- src/controllers/posts/deletePostController.ts
+++ src/controllers/posts/deletePostController.ts
@@ -1,8 +1,23 @@
 import { NextFunction, Response } from 'express';
-import { CustomError, addUserSchema } from '../../../utils';
-import { AddUserRequest, User } from '../../../utils/types';
 import { deletePostQuery, getPostByIdQuery } from '../../database';
-const deletePostController = async (req:, res: Response, next: NextFunction) => {
+import { CustomError, deletePostSchema } from '../../utils';
+import { DeletePostRequest } from '../../utils/types/requests';
 
-}
+const deletePostController = async (req: DeletePostRequest, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    await deletePostSchema.validateAsync({ id });
+    const { rowCount } = await getPostByIdQuery({ userId: +(id) });
+    if (!rowCount) throw new CustomError('Post not found', 404);
+    await deletePostQuery({ id: +(id) });
+    res.status(200).json({
+      data: {
+        message: 'Post deleted successfully',
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
+export default deletePostController;
