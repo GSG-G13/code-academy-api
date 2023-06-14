228d59665ab70c1d1ae057b688c60d5876641540
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-06-14 12:54:04 +0300

    feat: create edit post query

---
+++ src/database/query/posts/editPostQuery.ts
@@ -0,0 +1,12 @@
+import { EditPost } from '../../../utils';
+import connection from '../../config/connection';
+
+const editPostQuery = ({ content, postId }: EditPost) => {
+  const sql = {
+    text: 'UPDATE posts SET content = $1 WHERE post_id = $2',
+    values: [content, postId],
+  };
+  return connection.query(sql);
+};
+
+export default editPostQuery;
