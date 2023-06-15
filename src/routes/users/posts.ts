451d4882632193f7433e08b6854d2fdf58303db9
Nour-Agha99 <temoagha@gmail.com>
2023-06-15 03:15:12 +0300

    feat: add getPostsCohort router

--- src/routes/users/posts.ts
+++ src/routes/users/posts.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { getAllPostsController } from '../../controllers';
+import { getPostsCohortController, getAllPostsCommunityController } from '../../controllers';
 import checkAuth from '../../middlewares';
 
 const postsRouter: Router = express.Router();
 
 postsRouter.use(checkAuth);
-postsRouter.get('/', getAllPostsController);
+postsRouter.get('/', getAllPostsCommunityController);
+postsRouter.get('/:stringId', getPostsCohortController);
 
 export default postsRouter;
