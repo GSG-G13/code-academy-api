2190bdcfa0219988493bd3958c8db1e95ba22419
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-06-15 11:10:59 +0300

    fix: update editPostSchema with adding postId, and delete cohortId and isPublic

--- src/utils/validation/posts/editPostSchema.ts
+++ src/utils/validation/posts/editPostSchema.ts
@@ -1,18 +1,15 @@
 import Joi from 'joi';
 
 const editPostSchema = Joi.object({
+  postId: Joi.number().required().messages({
+    'number.base': 'Post id must be a number',
+    'number.empty': 'Post id cannot be empty',
+    'any.required': 'Post id is required',
+  }),
   content: Joi.string().min(2).required().messages({
     'string.empty': 'Post content cannot be empty',
     'string.min': 'Post content should have a minimum length of 2',
     'any.required': 'Post content is required',
-  }),
-  isPublic: Joi.boolean().required().messages({
-    'string.empty': 'Post visibility cannot be empty',
-    'any.required': 'Post visibility is required',
-  }),
-  cohortId: Joi.number().required().messages({
-    'string.empty': 'Cohort cannot be empty',
-    'any.required': 'Cohort visibility is required',
   }),
 });
 
