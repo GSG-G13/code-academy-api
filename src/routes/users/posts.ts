805b9435e901893d7555b1d0dc4b8575d000a91a
Nour-Agha99 <temoagha@gmail.com>
2023-06-15 11:03:24 +0300

    fix: edit route end point for cohort posts

--- src/routes/users/posts.ts
+++ src/routes/users/posts.ts
@@ -6,6 +6,6 @@ const postsRouter: Router = express.Router();
 
 postsRouter.use(checkAuth);
 postsRouter.get('/', getAllPostsCommunityController);
-postsRouter.get('/:stringId', getPostsCohortController);
+postsRouter.get('/cohort/:stringId', getPostsCohortController);
 
 export default postsRouter;
