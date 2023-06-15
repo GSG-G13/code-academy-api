551b5c3339a8df6bceedf1b6155a3c974baea08e
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-06-15 11:12:20 +0300

    fix: update editPostQuery to update updated_at column

--- src/database/query/posts/editPostQuery.ts
+++ src/database/query/posts/editPostQuery.ts
@@ -3,7 +3,7 @@ import connection from '../../config/connection';
 
 const editPostQuery = ({ content, postId }: EditPost) => {
   const sql = {
-    text: 'UPDATE posts SET content = $1 WHERE id = $2',
+    text: 'UPDATE posts SET content = $1, updated_at = CURRENT_TIMESTAMP WHERE id = $2 RETURNING *',
     values: [content, postId],
   };
   return connection.query(sql);
