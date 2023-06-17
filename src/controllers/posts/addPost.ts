f93467fd688465eecc3c20f4c5c73f305d2ee240
Batrawi <batrawi2000forever@gmail.com>
2023-06-17 21:37:50 +0300

    refactor: include authorization checks and handle cohort restrictions in addPost

--- src/controllers/posts/addPost.ts
+++ src/controllers/posts/addPost.ts
@@ -7,9 +7,23 @@ const addPostController = async (req: AddPostRequest, res: Response, next: NextF
     if (!req.user) throw new CustomError('Unauthorized', 401);
 
     const { content, image, isPublic, cohortId } = req.body;
+
     await validateAddPostSchema.validateAsync({ content, isPublic, cohortId });
-    const { id } = req.user;
-    const { rows } = await addPostQuery({ userId: id, content, image, isPublic, cohortId });
+    const { id, roles, isAdmin } = req.user;
+    if (!isAdmin
+      && (roles === undefined || !roles.some((role) => role.cohort_id === cohortId))
+      && !isPublic
+    ) {
+      throw new CustomError('Unauthorized', 401);
+    }
+
+    const { rows } = await addPostQuery({
+      userId: id,
+      content,
+      image,
+      isPublic,
+      cohortId: isPublic ? null : cohortId,
+    });
 
     res.status(200).json({ message: 'post added success', post: rows });
   } catch (err) {
