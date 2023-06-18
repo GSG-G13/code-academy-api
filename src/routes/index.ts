1480cd92453d3758e2a658cf5c8e944ac23c6063
Batrawi <batrawi2000forever@gmail.com>
2023-06-18 23:24:34 +0300

    feat: create likes router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authRouter, userRouter, cohortsRouter } from './admin';
 import postsRouter from './posts';
+import likesRouter from './likes';
 
 const router: Router = express.Router();
 
@@ -8,5 +9,6 @@ router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/cohorts', cohortsRouter);
 router.use('/posts', postsRouter);
+router.use('/likes', likesRouter);
 
 export default router;
