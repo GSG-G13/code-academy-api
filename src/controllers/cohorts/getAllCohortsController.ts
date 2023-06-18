643036e0378fce2d9046393b013180b961255b20
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-06-18 15:40:52 +0300

    feat: create get all cohorts router

--- src/controllers/cohorts/getAllCohortsController.ts
+++ src/controllers/cohorts/getAllCohortsController.ts
@@ -1,7 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { getAllCohortsQuery } from '../../database';
+import { RequestWithDecoded } from '../../utils';
 
-const getAllCohortsController = async (req: Request, res: Response, next: NextFunction) => {
+const getAllCohortsController = async (
+  req: RequestWithDecoded,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { rows } = await getAllCohortsQuery();
     res.json({
--- src/routes/admin/cohorts.ts
+++ src/routes/admin/cohorts.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { addCohortController } from '../../controllers';
+import { addCohortController, getAllCohortsController } from '../../controllers';
 import checkAuth from '../../middlewares';
 
 const cohortsRouter: Router = express.Router();
 
 cohortsRouter.use(checkAuth);
 cohortsRouter.post('/', addCohortController);
+cohortsRouter.get('/', getAllCohortsController);
 
 export default cohortsRouter;
