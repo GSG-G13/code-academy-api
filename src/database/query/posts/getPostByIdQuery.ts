517a0dc9015ab337998adc7ab668a66e7554ad2d
Nour-Agha99 <temoagha@gmail.com>
2023-06-18 20:50:57 +0300

    fix: edit getPostByIdQuery

--- src/database/query/posts/getPostByIdQuery.ts
+++ src/database/query/posts/getPostByIdQuery.ts
@@ -3,7 +3,7 @@ import connection from '../../config/connection';
 
 const getPostByIdQuery = ({ id }: PostIdQueryArgs) => {
   const sql = {
-    text: 'SELECT * FROM posts WHERE id = $1',
+    text: 'SELECT users.id AS "userId", users.username AS "username", users.avatar AS "avatar", posts.id AS "id", posts.image AS "image", posts.content AS "content", posts.created_at AS "createdAt", posts.updated_at AS "updatedAt", COUNT(likes.id) AS "likesCount", COUNT(comments.id) AS "commentsCount", COUNT(saved_posts.id) AS "savesCount" FROM posts JOIN users ON posts.user_id = users.id LEFT JOIN likes ON likes.post_id = posts.id LEFT JOIN comments ON comments.post_id = posts.id LEFT JOIN users AS comment_users ON comments.user_id = comment_users.id LEFT JOIN saved_posts ON saved_posts.post_id = posts.id WHERE posts.id = $1 GROUP BY users.id, posts.id ORDER BY posts.id DESC',
     values: [id],
   };
   return connection.query(sql);
