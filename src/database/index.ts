48c550e6cc2291de0448bf20bd93232225559d17
= <shathaamin63@gmail.com>
2023-06-18 13:18:14 +0300

    fix: handle error

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,6 +12,7 @@ import {
   editPostQuery,
   deletePostQuery,
   getPostByIdQuery,
+  getSingleCohortQuery,
 } from './query';
 
 export {
@@ -28,4 +29,5 @@ export {
   editPostQuery,
   deletePostQuery,
   getPostByIdQuery,
+  getSingleCohortQuery,
 };
