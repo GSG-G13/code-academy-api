f7cfd426d6aefd8a3c0c5784229e9f903d1aebdf
= <shathaamin63@gmail.com>
2023-06-18 12:53:29 +0300

    feat: add get single cohort query

---
+++ src/database/query/cohorts/getSingleCohortQuery.ts
@@ -0,0 +1,21 @@
+import connection from '../../config/connection';
+
+interface GetSingleCohortQueryArgs {
+  id: number;
+}
+
+const getSingleCohortQuery = ({ id }: GetSingleCohortQueryArgs) => {
+  const sql = {
+    // eslint-disable-next-line quotes
+    text: `SELECT c.*, u.id, u.username, u.avatar FROM cohorts c
+            JOIN user_roles ur ON c.id = ur.cohort_id
+            JOIN users u ON ur.user_id = u.id
+            WHERE c.id = 2
+            group by c.id, u.id`,
+    values: [id],
+  };
+
+  return connection.query(sql);
+};
+
+export default getSingleCohortQuery;
