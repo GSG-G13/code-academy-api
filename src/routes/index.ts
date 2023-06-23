1ebe42fefd1cd21c4025fe965041e4880256c6c7
Nour-Agha99 <temoagha@gmail.com>
2023-06-24 01:41:53 +0300

    feat: add getMembers router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authRouter, userRouter, cohortsRouter } from './admin';
 import postsRouter from './posts';
 import userCohortsRouter from './cohorts';
+import getMembersRouter from './users';
 
 const router: Router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/users', userRouter);
 router.use('/cohorts', cohortsRouter);
 router.use('/posts', postsRouter);
 router.use('/cohorts', userCohortsRouter);
+router.use('/members', getMembersRouter);
 
 export default router;
