7bbc4b9fb708e2a565459698e64c2682fd3d5518
Nour-Agha99 <temoagha@gmail.com>
2023-06-25 11:40:44 +0300

    fix: refactor

--- src/controllers/admin/auth/logoutController.ts
+++ src/controllers/admin/auth/logoutController.ts
@@ -3,7 +3,12 @@ import { RequestWithDecoded } from '../../../utils';
 
 const logoutController = async (req: RequestWithDecoded, res: Response, next: NextFunction) => {
   try {
-    res.clearCookie('token').json({ message: 'Logged Out Successfully' });
+    res.clearCookie('token').json({
+      error: false,
+      data: {
+        message: 'Logged Out Successfully',
+      },
+    });
   } catch (err) {
     next(err);
   }
