952843073570d30a659da97c00afe360381ed023
Batrawi <batrawi2000forever@gmail.com>
2023-06-25 11:26:41 +0300

    refactor: edit addPost name in controlller folder

--- src/controllers/posts/index.ts
+++ src/controllers/posts/index.ts
@@ -1,6 +1,6 @@
 import getPublicPostsController from './getPublicPostsController';
 import getCohortPostsController from './getCohortPostsController';
-import addPostController from './addPost';
+import addPostController from './addPostController';
 import editPostController from './editPostController';
 import deletePostController from './deletePostController';
 import getSinglePostController from './getSinglePostController';
