9b5384562355651c6052a0278cd2279b944a0483
Batrawi <batrawi2000forever@gmail.com>
2023-07-02 20:34:21 +0300

    refactor: edit getSingleMember query

--- src/database/query/users/getSingleMemberQuery.ts
+++ src/database/query/users/getSingleMemberQuery.ts
@@ -3,10 +3,33 @@ import { MemberIdQueryArgs } from '../../../utils';
 
 const getSingleMemberQuery = ({ memberId }: MemberIdQueryArgs) => {
   const sql = {
-    text: 'select * from users where id = $1',
+    text: `
+      SELECT
+        users.full_name,
+        users.username,
+        users.email,
+        users.github,
+        users.linkedin,
+        users.avatar,
+        TO_CHAR(users.date_of_birth, 'YYYY-MM-DD') AS date_of_birth,
+        career_status.name AS career_status,
+        users.gender,
+        users.is_active,
+        array_agg(user_roles.cohort_id) AS cohort_ids
+      FROM
+        users
+      JOIN
+        career_status ON users.career_status_id = career_status.id
+      LEFT JOIN
+        user_roles ON user_roles.user_id = users.id
+      WHERE
+        users.id = $1
+      GROUP BY
+        users.id,
+        career_status.id
+    `,
     values: [memberId],
   };
   return connection.query(sql);
 };
-
 export default getSingleMemberQuery;
