e4e39cd18d9a62c6a9ade0054df9f27cf577879b
= <shathaamin63@gmail.com>
2023-07-02 16:22:29 +0300

    feat: add get all cohorts query

--- src/database/query/cohorts/getAllCohortsQuery.ts
+++ src/database/query/cohorts/getAllCohortsQuery.ts
@@ -1,8 +1,14 @@
 import connection from '../../config';
+import { AllCohortsQueryArgs } from '../../../utils';
 
-const getAllCohortsQuery = () => {
+const getAllCohortsQuery = ({ offset }: AllCohortsQueryArgs) => {
   const sql = {
-    text: 'SELECT c.id, c.name, c.thumbnail, COUNT(u.user_id) AS members FROM cohorts c INNER JOIN user_roles u ON c.id = u.cohort_id GROUP BY c.id, c.name, c.thumbnail;',
+    text: `SELECT c.id, c.name, c.thumbnail, COUNT(u.user_id) AS members 
+           FROM cohorts c 
+           LEFT JOIN user_roles u ON c.id = u.cohort_id 
+           GROUP BY c.id, c.name, c.thumbnail
+           LIMIT 15 OFFSET $2;`,
+    values: [offset],
   };
   return connection.query(sql);
 };
