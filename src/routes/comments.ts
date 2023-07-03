766eb70544177341af47bed2669a99f9ea895573
Nour-Agha99 <temoagha@gmail.com>
2023-07-03 19:53:06 +0300

    feat: get Comments single post [route]

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { commentController } from '../controllers';
+import { addCommentController, getCommentsController } from '../controllers';
 import checkAuth from '../middlewares';
 
 const commentsRouter: Router = express.Router();
 
 commentsRouter.use(checkAuth);
-commentsRouter.post('/', commentController);
+commentsRouter.get('/', getCommentsController);
+commentsRouter.post('/', addCommentController);
 
 export default commentsRouter;
