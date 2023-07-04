dea43f797f443dbf5a1ed78b90053d56fd74af3d
= <shathaamin63@gmail.com>
2023-07-04 14:20:21 +0300

    feat: add get  my cohorts query

---
+++ src/database/query/cohorts/getMyCohortsQuery.ts
@@ -0,0 +1,29 @@
+import connection from '../../config';
+import { getMyCohortsQueryArgs } from '../../../utils';
+
+const getMyCohortsQuery = ({ offset, userId }: getMyCohortsQueryArgs) => {
+  const sql = {
+    text: `SELECT c.id ,c.name AS cohort_name, COUNT(ur.user_id) AS members,
+    c.thumbnail,
+    c.start_date,
+    c.end_date,
+    r.name AS role_name
+  FROM
+    cohorts c
+
+  INNER JOIN
+    user_roles ur ON c.id = ur.cohort_id
+  INNER JOIN
+    roles r ON ur.role_id = r.id
+  WHERE
+    ur.user_id =$1
+    GROUP BY c.id, c.name, c.thumbnail,c.start_date ,c.end_date, r.name
+    ORDER BY c.id
+
+    LIMIT 15 OFFSET $2;`,
+    values: [userId, offset],
+  };
+  return connection.query(sql);
+};
+
+export default getMyCohortsQuery;
