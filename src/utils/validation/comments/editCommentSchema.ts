cddb01da266a79fb80c4da69c39d870364c0b7f7
Nour-Agha99 <temoagha@gmail.com>
2023-07-04 14:47:01 +0300

    fix: edit error status [controller]

--- src/utils/validation/comments/editCommentSchema.ts
+++ src/utils/validation/comments/editCommentSchema.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const editCommentSchema = Joi.object({
-  commentId: Joi.number().required().messages({
+  id: Joi.number().required().messages({
     'number.base': 'Comment id must be a number',
     'number.empty': 'Comment id cannot be empty',
     'any.required': 'Comment id is required',
