d65e175509f4cf43d69ab631398d5198de58b0b2
Nour-Agha99 <temoagha@gmail.com>
2023-07-07 02:56:51 +0300

    fix: edit getUserByEmailQuery [query]

--- src/database/query/auth/getUserByEmailQuery.ts
+++ src/database/query/auth/getUserByEmailQuery.ts
@@ -6,7 +6,12 @@ interface Args {
 
 const getUserByEmailQuery = ({ email }: Args) => {
   const sql = {
-    text: 'SELECT * FROM users WHERE email = $1',
+    text: `
+    SELECT users.id, users.full_name,users.password , users.username, users.email, roles.name AS role
+    FROM users
+    JOIN user_roles ON users.id = user_roles.user_id
+    JOIN roles ON user_roles.role_id = roles.id
+    WHERE email = $1`,
     values: [email],
   };
   return connection.query(sql);
