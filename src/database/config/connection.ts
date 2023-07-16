7957aa6cf3daad9491821094175dce3c3c66e95c
Nour-Agha99 <temoagha@gmail.com>
2023-07-16 16:15:46 +0300

    fix: edit connection

--- src/database/config/connection.ts
+++ src/database/config/connection.ts
@@ -4,9 +4,7 @@ import { DEV_DB_URL, DATABASE_URL, NODE_ENV } from '../../config';
 
 const options = {
   connectionString: NODE_ENV === 'production' ? DATABASE_URL : DEV_DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 };
 
 const connection = new Pool(options);
