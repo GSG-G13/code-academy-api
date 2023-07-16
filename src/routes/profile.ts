3870197f2bbdeba193b1dbc7000bf261531d2b4c
Nour-Agha99 <temoagha@gmail.com>
2023-07-16 15:57:10 +0300

    feat: add user information [Router]

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { getAllPostsByUserIdController } from '../controllers';
+import { getAllPostsByUserIdController, getPersonalInfo } from '../controllers';
 import checkAuth from '../middlewares';
 
 const profileRouter: Router = express.Router();
 
 profileRouter.use(checkAuth);
+profileRouter.get('/', getPersonalInfo);
 profileRouter.get('/posts', getAllPostsByUserIdController);
 
 export default profileRouter;
