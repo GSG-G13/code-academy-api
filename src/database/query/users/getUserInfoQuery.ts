8b472c330155954f65b6ad4386fc2a0332a78add
Nour-Agha99 <temoagha@gmail.com>
2023-07-17 13:00:24 +0300

    fix: refactor

--- src/database/query/users/getUserInfoQuery.ts
+++ src/database/query/users/getUserInfoQuery.ts
@@ -5,13 +5,13 @@ const getUserInfoQuery = ({ id }: PostIdQueryArgs) => {
   const sql = {
     text: `
       SELECT 
-        users.id, users.full_name, users.username, users.avatar,
-        TO_CHAR(users.date_of_birth, 'YYYY-MM-DD') AS DOB,
-        users.gender, roles.name as role
-      FROM users
-          JOIN user_roles ON users.id = user_roles.user_id
-          JOIN roles ON user_roles.role_id = roles.id
-      WHERE users.id = $1
+        users.id, users.full_name, users.username, users.avatar, TO_CHAR(users.date_of_birth, 'YYYY-MM-DD') AS DOB, users.gender, career_status.name as role
+      FROM 
+        users
+      JOIN 
+        career_status ON users.career_status_id = career_status.id
+      WHERE 
+        users.id = $1
     `,
     values: [id],
   };
